8cf997f162f2bcdff02e313a3178776e09aceb08
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-27 05:15:12 -0300

    Feat: Creating and Separating the Routes of our application

--- src/routes/sale.routes.jsx
+++ src/routes/sale.routes.jsx
@@ -3,16 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Product } from '../pages/Product';
 import { NotFound } from '../pages/NotFound';
-import { Suppliers } from '../pages/Suppliers';
 import { SalesReport } from '../pages/SalesReport';
 
-export function AppRoutes() {
+export function SaleRoutes() {
   return (
     <Routes>
       <Route path="/" element={< Home />} />
       <Route path="/product" element={<Product />} />
       <Route path="/sales-report" element={<SalesReport />} />
-      <Route path="/suppliers" element={<Suppliers />} />
 
       <Route path="*" exact={true} element={<NotFound />} />
     </Routes>
