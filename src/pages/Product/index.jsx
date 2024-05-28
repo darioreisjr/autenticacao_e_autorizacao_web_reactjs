d1aae887767f9b329e5ac5a04ee59e0011ce6a4f
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-28 04:59:40 -0300

    Feat: Filtering the Register Product button with access to each user

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,10 +1,14 @@
 import { useNavigate } from "react-router-dom";
 
+import { useAuth } from "../../hooks/auth";
+import {USER_ROLE} from "../../utils/roles"
+
 import { Container, Header, Item } from "./styles";
 import { Button } from '../../components/Button';
 
 export function Product() {
   const navigate = useNavigate();
+  const {user} = useAuth();
 
   const products = Array(20)
     .fill({ name: 'Produto' })
@@ -16,7 +20,7 @@ export function Product() {
         <h1>Produtos</h1>
 
         <nav>
-          <Button title="Cadastrar" />
+          { [USER_ROLE.ADMIN].includes(user.role) && <Button title="Cadastrar" />}
           <Button title="Voltar" onClick={() => navigate('/')} />
         </nav>
       </Header>
