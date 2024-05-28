d0be0c49ef03fb3d0eda1112489edb80f7aa51c0
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-28 04:44:25 -0300

    Feat: Filtering the Feature Options on the Home with each user’s profile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,46 @@
-import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
+import { FiTruck, FiTag, FiShoppingCart } from "react-icons/fi";
+
+import { useAuth } from "../../hooks/auth";
+import { USER_ROLE } from "../../utils/roles";
 
 import { Container } from "./styles";
-import { Feature } from '../../components/Feature';
-import { Header } from '../../components/Header';
+import { Feature } from "../../components/Feature";
+import { Header } from "../../components/Header";
 
 export function Home() {
+  const { user } = useAuth();
+
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+
+        {/* {[USER_ROLE.ADMIN, USER_ROLE.SALE].includes(user.role) && (
+          <>
+            {user.role === USER_ROLE.ADMIN && (
+              <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
+            )}
+            <Feature
+              title="Relatório de vendas"
+              icon={FiShoppingCart}
+              to="/sales-report"
+            />
+          </>
+        )} */}
+
+        {[USER_ROLE.ADMIN].includes(user.role) && (
+          <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
+        )}
+        {[USER_ROLE.ADMIN, USER_ROLE.SALE].includes(user.role) && (
+          <Feature
+            title="Relatório de vendas"
+            icon={FiShoppingCart}
+            to="/sales-report"
+          />
+        )}
       </main>
     </Container>
-  )
+  );
 }
