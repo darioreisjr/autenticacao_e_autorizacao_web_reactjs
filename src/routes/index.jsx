c0f28a59d1752ea71bd322037f5b1e6fc1778169
Dario Reis <guttyerresjunior1995@gmail.com>
2024-06-03 05:32:10 -0300

    Feat: Validating User when logging into the application

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,32 +1,36 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 import { useAuth } from "../hooks/auth";
-import { USER_ROLE } from '../utils/roles';
+import { USER_ROLE } from "../utils/roles";
 
-import { CustomerRoutes } from './customer.routes';
-import { AdminRoutes } from './admin.routes';
-import { SaleRoutes } from './sale.routes';
-import { AuthRoutes } from './auth.routes';
+import { CustomerRoutes } from "./customer.routes";
+import { AdminRoutes } from "./admin.routes";
+import { SaleRoutes } from "./sale.routes";
+import { AuthRoutes } from "./auth.routes";
+import { useEffect } from "react";
+import { api } from "../services/api";
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
-  function AccessRoute(){
-    switch(user.role){
+  useEffect(() => {
+    api.get("/users/validated").catch((error) => signOut());
+  }, []);
+
+  function AccessRoute() {
+    switch (user.role) {
       case USER_ROLE.ADMIN:
-        return <AdminRoutes />
+        return <AdminRoutes />;
       case USER_ROLE.CUSTOMER:
-        return <CustomerRoutes />
+        return <CustomerRoutes />;
       case USER_ROLE.SALE:
-        return <SaleRoutes />
+        return <SaleRoutes />;
       default:
-        return <CustomerRoutes />
+        return <CustomerRoutes />;
     }
   }
 
   return (
-    <BrowserRouter>
-      {user ? <AccessRoute /> : <AuthRoutes />}
-    </BrowserRouter>
+    <BrowserRouter>{user ? <AccessRoute /> : <AuthRoutes />}</BrowserRouter>
   );
 }
